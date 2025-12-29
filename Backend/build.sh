a39f9c6d575136469b39db7bab16e07e2acf9f84
olowojude <olowojude73@gmail.com>
2025-12-29 11:14:59 +0100

    update build.sh

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,6 +1,1 @@
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt && python manage.py collectstatic --noinput && python manage.py migrate
