6b7b999918f8cb5347819a28a4169a49d4acbab8
olowojude <olowojude73@gmail.com>
2026-01-06 05:37:07 +0100

    remove clear db file

--- Backend/reset_database.py
+++ Backend/reset_database.py
@@ -1,15 +1,15 @@
-import os
-import django
+# import os
+# import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodflex.settings')
-django.setup()
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodflex.settings')
+# django.setup()
 
-from django.db import connection
+# from django.db import connection
 
-with connection.cursor() as cursor:
-    cursor.execute("DROP SCHEMA public CASCADE;")
-    cursor.execute("CREATE SCHEMA public;")
-    cursor.execute("GRANT ALL ON SCHEMA public TO postgres;")
-    cursor.execute("GRANT ALL ON SCHEMA public TO public;")
+# with connection.cursor() as cursor:
+#     cursor.execute("DROP SCHEMA public CASCADE;")
+#     cursor.execute("CREATE SCHEMA public;")
+#     cursor.execute("GRANT ALL ON SCHEMA public TO postgres;")
+#     cursor.execute("GRANT ALL ON SCHEMA public TO public;")
     
-print("✅ Database cleared successfully!")
+# print("✅ Database cleared successfully!")
